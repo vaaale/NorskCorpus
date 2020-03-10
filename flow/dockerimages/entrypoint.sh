6a469abb41791fd68b118f3928671301ba9bb81b
alex <alexander.vaagan@gmail.com>
2020-03-10 08:11:36 +0100

    Added flow framework

--- flow/dockerimages/entrypoint.sh
+++ flow/dockerimages/entrypoint.sh
@@ -16,6 +16,13 @@ echo $PWD
 find . -type f -not -name '_source.tar.gz' -delete
 echo "Extracting _source.tar.gz"
 tar zxvf _source.tar.gz
+retVal=$?
+if [ $retVal -ne 0 ]; then
+    echo "Error extracting files"
+    return -1
+elif
+    rm _source.tar.gz
+fi
 echo "Done"
 
 echo "Install requirements"
@@ -24,7 +31,11 @@ pip3 install -r requirements.txt
 echo "Staring $2"
 python3 ${@:2}
 
-RET=$?
-
-echo "Process exited with status ${RET}"
+retVal=$?
+if [ $retVal -ne 0 ]; then
+    echo "Error!"
+    return -1
+elif
+    echo "Success!"
+fi
 
